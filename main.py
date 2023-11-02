13d9bf77e413103fdb9833ee0a8c38778e05f778
Harold Ship <harold@il.ibm.com>
2023-11-02 16:12:47 -0400

    run with 2 file parameters

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
+import errno, os, sys
+
 import simpy
 from typing import Callable
 from functools import partial
-import numpy as np
 import numpy.random as npr
-import matplotlib.pyplot as plt
 import tomli
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import pandas as pd
 
 
 def exponential_time(lam):
@@ -20,7 +16,6 @@ def service(env: simpy.Environment, q: simpy.Resource, reqno: int, stime: float)
     with q.request() as request:
         yield request
         yield env.timeout(stime)
-        # print(f'stime: {reqno} {stime} {env.now}')
 
 
 def arrival_process(env: simpy.Environment, q: simpy.Resource, Q0:int, arrival_time: Callable, service_time: Callable):
@@ -31,11 +26,9 @@ def arrival_process(env: simpy.Environment, q: simpy.Resource, Q0:int, arrival_t
         reqno += 1
         yield env.timeout(atime)
         env.process(service(env, q, reqno, stime))
-        # print(f'arrival: {reqno} {atime} {env.now}')
 
 
 def run_simulation(mc: int, T:float, Q0:int, interarrival_time: Callable, service_time: Callable):
-    # print(f'iteration: {mc}')
     env = simpy.Environment()
     q = simpy.Resource(env, 1)
     for reqno in range(Q0):
@@ -64,54 +57,76 @@ def log(logs: list, env: simpy.Environment, mc: int, T: float, delta_t: float, q
 
 
 if __name__ == '__main__':
-    MC = 100
     npr.seed(1)
-    with open("queue-params-example.toml", "rb") as f:
+    if len(sys.argv) != 3:
+        print(f'Usage: {os.path.basename(sys.argv[0])} <param-file> <controls-file>', file=sys.stderr)
+        sys.exit(errno.EINVAL)
+
+    param_filename, controls_filename = sys.argv[1:]
+
+    with open(param_filename, "rb") as f:
         queue_params = tomli.load(f)
         Q0 = queue_params['Q0']
         lam = queue_params['lam']
         mu = queue_params['mu']
         T = queue_params['T']
-    with open("queue-controls-example.toml", "rb") as f:
+        
+    with open(controls_filename, "rb") as f:
         queue_controls = tomli.load(f)
         delta_t = queue_controls['delta_t']
         eta = queue_controls['eta']
+        MC = queue_controls['MC']
+
+    print(f'Running {MC} simulations of queue with lamdba={lam} mu={mu} Q0={Q0} over time {T}')
+    print(f'    Sampling every {delta_t} and using control {eta}.')
 
     interarrival_time = partial(exponential_time, lam)
     service_time = partial(exponential_time, mu*eta)
 
     run_simulations(MC, T, Q0, interarrival_time, service_time)
 
-    mu_hats = list()
-    t_ests = list()
-    for mc in range(MC):
-        mclogs = logs[mc]
-        K = len(mclogs)
-        k1, k2 = sorted(npr.choice(K, 2, replace=False))
-        l1, l2 = mclogs[k1], mclogs[k2]
-        q1, q2 = l1[3], l2[3]
-        t1, t2 = l1[2], l2[2]
-        t_est = t2 - t1
-        mu_hat = -1.0/(eta*(t_est))*(q2-q1-lam*(t_est))
-        mu_hats.append(mu_hat)
-        t_ests.append(t_est)
-    print(np.mean(mu_hats))
-    print(np.var(mu_hats))
-    print(np.std(mu_hats))
+    out_dir = 'output'
+    os.makedirs(out_dir, exist_ok=True)
 
-    fig, (p1, p2) = plt.subplots(1, 2)
-    p1.hist(mu_hats, bins=20)
-    p1.set_title('Distribution of Service Rate Estimate')
-    p1.axvline(mu, color='red')
-    p1.axvline(lam, color='green')
-    p2.scatter(t_ests, mu_hats)
-    p2.axhline(mu, color='red')
-    p2.axhline(lam, color='green')
-    p2.set_title(f'Rate estimate for mu={mu}')
-    p2.set_xlabel('Time range t_2 - t_1')
-    p2.set_ylabel('Rate estimate mu_hat')
-    plt.tight_layout()
-    plt.show()
+    out_filename = f'{out_dir}/mu-{mu}--lam-{lam}--Q0-{Q0}--T-{T}--delta_t-{delta_t}--eta-{eta}.csv'
+    log_list = [log for mclogs in logs for log in mclogs]
+    log_df = pd.DataFrame(data=log_list, columns=('mc', 'k', 't_k', 'Q_k'))
+    log_df.to_csv(out_filename, index=False)
+
+    print(f'Results saved to {out_filename}')
+
+    # mu_hats = list()
+    # t_ests = list()
+    # for mc in range(MC):
+    #     mclogs = logs[mc]
+    #     K = len(mclogs)
+    #     k1, k2 = sorted(npr.choice(K, 2, replace=False))
+    #     l1, l2 = mclogs[k1], mclogs[k2]
+    #     q1, q2 = l1[3], l2[3]
+    #     t1, t2 = l1[2], l2[2]
+    #     t_est = t2 - t1
+    #     mu_hat = -1.0/(eta*(t_est))*(q2-q1-lam*(t_est))
+    #     mu_hats.append(mu_hat)
+    #     t_ests.append(t_est)
+    #
+    #
+    # print(np.mean(mu_hats))
+    # print(np.var(mu_hats))
+    # print(np.std(mu_hats))
+    #
+    # fig, (p1, p2) = plt.subplots(1, 2)
+    # p1.hist(mu_hats, bins=20)
+    # p1.set_title('Distribution of Service Rate Estimate')
+    # p1.axvline(mu, color='red')
+    # p1.axvline(lam, color='green')
+    # p2.scatter(t_ests, mu_hats)
+    # p2.axhline(mu, color='red')
+    # p2.axhline(lam, color='green')
+    # p2.set_title(f'Rate estimate for mu={mu}')
+    # p2.set_xlabel('Time range t_2 - t_1')
+    # p2.set_ylabel('Rate estimate mu_hat')
+    # plt.tight_layout()
+    # plt.show()
 
 
 
